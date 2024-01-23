01f47417b67526e024346411771028f79f9e1698
YaqubH <Yaqubhasann@gmail.com>
2024-01-22 23:03:35 -0800

    Search Function
    I added the city search function that helps search for any city in the world with a population over 1,000,000.

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9bbf4113c5mshd288e903b62afb3p1ddfa3jsn34123a108916",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
